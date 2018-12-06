9d9cfda11ff80e8e2039997d2a1a94fdde15711c
kumaraditya1999 <kumaraditya1999@gmail.com>
2018-12-06 16:27:20 +0530

    made the home page

---
+++ web page/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("#register-btn").click(function(e){
+
+		//post request handlers would be here
+		
+
+
+
+	});
+
+	$("#login-btn").click(function(){
+		//postrequest handler
+	});
+
+});
+
