69f950b60f9103a3ef75c1a055a8197b0424b01a
CircleJerkHug <devansh.prakhar@gmail.com>
2018-12-08 15:57:37 +0530

    Anime List Generated via BeautifulSoup

--- myanimelist.py
+++ myanimelist.py
@@ -2,35 +2,72 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url="https://myanimelist.net/topanime.php?type=bypopularity"
-source=requests.get(url)
 
-
-soup=BeautifulSoup(source.content,'lxml')
 json_file=open('anime_scrape.json','w')
 
+#Function to get url and return the data to scrape from it
+def make_soup(url):
+	source=requests.get(url)
+	soupdata=BeautifulSoup(source.content,'lxml')
+	return soupdata
 
-#Extracting the name of the anime
-for anime in soup.find_all('div',class_='di-ib clearfix'):
-	name=anime.a.text
-	print name
-	
-	json.dump(name,json_file)
-	print(' ')
+'''
+Iterating the loop from 1 to 30000 to store data from myanimelist
+where the url is  https://myanimelist.net/anime/(some number)/
+'''
 
-#Extracting the rating 
-rating=soup.find_all('span',class_='text on')
-for val in rating:
-	print val.text.strip()
-	json.dump(val.text.strip(),json_file)
+for index in range(1,30000):
+		try:
+			'''
+			Handling the exception when Error 404 shows up i.e page is not found
+			'''
+			r=requests.get("https://myanimelist.net/anime/"+str(index)+"/")		
+			r.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+				continue
+		soup=make_soup("https://myanimelist.net/anime/"+str(index)+"/")
+		'''
+		Function to store the major information about a anime
+		and storing the data in anime_scrape.json file
+		'''
+
+		def info_anime(soup):
+
+			#Extracting the name of the anime
+
+			anime=soup.find(name="span",attrs={"itemprop":"name"})
+			name=anime.text
+			print "Anime : "+name
+			json.dump(name,json_file)
 
 
-#extracting the description
 
-for des in soup.find_all('div',style='margin-top: 8px;margin-bottom: 10px;'):
-	description=des.div.text
-	print description
-	json.dump(description,json_file)
+			#Extracting the rating 
 
-json_file.close()
+			rating=soup.find(name="div",attrs={"class":"fl-l score"})
+			print "Rating : "+(rating.text.strip())
+			json.dump(rating.text.strip(),json_file)
+
+
+			#extracting the description
+
+			des=soup.find(name="span",attrs={"itemprop":"description"})
+			description=des.text
+			print "Description : "+description
+			json.dump(description,json_file)
+
+			#Extracting the Rank
+
+			rank=soup.find(name="span",attrs={"class":"numbers ranked"})
+			print rank.text
+
+			#Extracting number of episodes
+
+			ep=soup.find(name="div",attrs={"class":"spaceit"})
+			print ep.text
+
+		print info_anime(soup)
+
+if __name__ == '__main__':
+	info_anime(soup)
 
