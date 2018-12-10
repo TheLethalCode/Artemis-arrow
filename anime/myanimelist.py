b4ae09530be1008eae239e6a452fd7a13dfbdb70
thelethalcode <kousshikraj.raj@gmail.com>
2018-12-10 22:32:02 +0530

    Made variables universal

--- anime/myanimelist.py
+++ anime/myanimelist.py
@@ -1,69 +1,76 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-
-
-json_file=open('anime_scrape.json','w')
+import os
 
 #Function to get url and return the data to scrape from it
-def make_soup(url):
-	source=requests.get(url)
+
+ANIME_URL = "https://myanimelist.net/anime/"
+JSON_PATH = os.path.join(os.path.dirname(__file__),"anime.json")
+
+def make_soup(source):
 	soupdata=BeautifulSoup(source.content,'lxml')
 	return soupdata
 
-'''
-Function to store the major information about a anime
-and storing the data in anime_scrape.json file
-'''
-
 def info_anime(soup):
 
-	#Extracting the name of the anime
+	'''
+	Returns the information about an anime from its soup
+	'''
+	anime_list = {}
 
+	# Extracting the name of the anime
 	anime=soup.find(name="span",attrs={"itemprop":"name"})
 	name=anime.text
-	print ("Anime : "+name)
-	json.dump(name,json_file)
-
-
-
-	#Extracting the rating 
-
+	anime_list["name"]=name
+	
+	# Extracting the rating 
 	rating=soup.find(name="div",attrs={"class":"fl-l score"})
-	print ("Rating : "+(rating.text.strip()))
-	json.dump(rating.text.strip(),json_file)
+	anime_list["rating"]=float(rating.text.strip())
 
-
-	#extracting the description
-
+	# Extracting the description
 	des=soup.find(name="span",attrs={"itemprop":"description"})
 	description=des.text
-	print ("Description : "+description)
-	json.dump(description,json_file)
+	anime_list["description"]=description
 
-	#Extracting the Rank
-
+	# Extracting the Rank
 	rank=soup.find(name="span",attrs={"class":"numbers ranked"})
-	print (rank.text)
+	num = rank.text.find("#")+1
+	anime_list["rank"]=int(rank.text[num:])
 
-	#Extracting number of episodes
-
+	# Extracting number of episodes
 	ep=soup.find(name="div",attrs={"class":"spaceit"})
-	print (ep.text)
+	num=ep.text.find(" ")+2
+	anime_list["episodes"]=int(ep.text[num:-3])
 
+	return anime_list
+
+def gen_list(start,end):
+
+	"""
+	Function that generates a list of anime and its detail from start to end
+	"""
+
+	anime_ind = {}
+	for index in range(start,end):
+		print("Index",index)
+		try:
+			# Handling the exception when page is not found
+			
+			r=requests.get(ANIME_URL+str(index)+"/")		
+			r.raise_for_status()
+			soup = make_soup(r)
+			anime_ind[index]=info_anime(soup)
+
+		except requests.exceptions.HTTPError:
+			continue
+
+	return anime_ind	
 
 if __name__ == '__main__':
-	'''
-	Iterating the loop from 1 to 30000 to store data from myanimelist
-	where the url is  https://myanimelist.net/anime/(some number)/
-	'''
-	for index in range(1,30000):
-		try:
-			#Handling the exception when page is not found
-			r=requests.get("https://myanimelist.net/anime/"+str(index)+"/")		
-			r.raise_for_status()
-		except requests.exceptions.HTTPError as err:
-			continue
-		soup=make_soup("https://myanimelist.net/anime/"+str(index)+"/")
-		info_anime(soup)
+	
+	with open(JSON_PATH,'w') as json_file:
+		json.dump(gen_list(1,20),json_file)
+		json_file.close()
+	
 
