bf02a10276b08370c4f589b861ba9d67c667cea8
thelethalcode <kousshikraj.raj@gmail.com>
2018-12-19 21:07:17 +0530

    Finalised Anime Downloading

--- anime/anime_download.py
+++ anime/anime_download.py
@@ -1,5 +1,4 @@
 import os
-from decouple import config
 from bs4 import BeautifulSoup                               
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,57 +12,63 @@ from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
-usernameStr=config("KISSANIME_USERNAME")
-passwordStr=config("KISSANIME_PASSWORD")
+usernameStr=os.getenv("KISSANIME_USERNAME")
+passwordStr=os.getenv("KISSANIME_PASSWORD")
+
+KISSANIME_URL = "https://kissanime.ru/"
 
 # a function to load chromedriver onto path
 def SetupSelenium():
-
     options = Options()
-    # to execute without launching browser. comment this if you are debugging
-    options.set_headless(True)
+    options.set_headless(True) # Without opening the browser
     firefox_capabilities = DesiredCapabilities.FIREFOX
-    # to load just the dom elements
     firefox_capabilities['pageLoadStrategy'] = "eager"
-    geckodriver = "/home/jeyasurya/Documents/bots/" # change this according to the location of your gecko driver
-    os.environ["webdriver.firefox.driver"] = geckodriver
-    browser = webdriver.Firefox(geckodriver,options = options)
+    browser = webdriver.Firefox(options = options)
     return browser
 
-# searches the string in kissanime and returns results
+# searches the string in kissanime and returns results for the anime
 # {status:indicates sucess or failure, links:[]}    
-def SearchAnime(searchstring):
+
+def SearchAnime(searchString):
+
+    KISSANIME_SEARCH = KISSANIME_URL + "Search/Anime"
+
     browser = SetupSelenium()
     resp = {
         "status" : "",
         "links" : []
     }
+
     try:
-        browser.get("https://kissanime.ru/")
+        browser.get(KISSANIME_URL)
         wait = WebDriverWait(browser, 30)
-        # Save the window opener (current window, do not mistaken with tab... not the same)
+        
+        # Save the window opener of the current window
         main_window = browser.current_window_handle
-        # wait until the page loads and locate the search bar and search
+
+        # Wait until the page loads and locate the search bar and search
         wait.until(EC.presence_of_element_located((By.ID,"keyword")))
         wait.until(EC.visibility_of_element_located((By.ID,"keyword")))
         searchInput = browser.find_element_by_id("keyword")
-        searchInput.send_keys(searchstring)
+        searchInput.send_keys(searchString)
         searchButton = browser.find_element_by_id("imgSearch")
         searchButton.click()
 
         # Put focus on current window which will, in fact, put focus on the current visible tab
         browser.switch_to_window(main_window)
-        # close any ads
-        while browser.current_url!="https://kissanime.ru/Search/Anime":
+        
+        # Close any ads
+        while browser.current_url != KISSANIME_SEARCH:
             ActionChains(browser).send_keys(Keys.CONTROL + 'w').perform()
 
         soup = BeautifulSoup(browser.page_source, 'html.parser')
         
         if len(soup.find_all('table')) == 0:
             return None
+
         table = soup.find_all('table')[0]
         links = table.find_all('a')
-        print(links)
+        # print(links)
         Anime_list = []
         for link in links:
             if "episode" not in link.getText().strip().lower():
@@ -73,24 +78,32 @@ def SearchAnime(searchstring):
                 }
                 Anime_list.append(Anime)
         browser.quit()
+
         if len(Anime_list)!=0:
             resp["status"]="success"
             resp["links"] = Anime_list
             return resp
+
         else:
             resp["status"]="not found"
             resp["links"] = []
             return resp
-    except TimeoutException as e:
+
+    except TimeoutException:
         print("search timed out")
         resp["status"] = "timed out"
         return resp
     
 # function to login into kissanime
 def login(browser):
+
+    KISSANIME_LOGIN = KISSANIME_URL + "login"
+
     try:
+
+        # Login using environment username and password
         main_window = browser.current_window_handle
-        browser.get("https://kissanime.ru/login")
+        browser.get(KISSANIME_LOGIN)
         wait = WebDriverWait(browser, 30)
         wait.until(EC.presence_of_element_located((By.ID,"username")))
         wait.until(EC.visibility_of_element_located((By.ID,"username")))
@@ -100,38 +113,46 @@ def login(browser):
         passwordInput.send_keys(passwordStr)
         loginButton = browser.find_element_by_id("btnSubmit")
         loginButton.click()
+
     except TimeoutException:
         print("login timed out")
         return False
+    
     browser.implicitly_wait(5)
-    # Put focus on current window which will put focus on the current visible tab
+    
+    # Put focus on current window which will put focus 
+    # on the current visible tab
     browser.switch_to_window(main_window)
-    #confirm login was successful
-    print(browser.current_url)
-    if browser.current_url=="https://kissanime.ru/":
+    
+    # confirm login was successful
+    if browser.current_url == KISSANIME_URL:
         return True
     else:
         return False
 
-# returns the rapidvideo download links of the given link in a dictionary
+# returns the rapidvideo download links of all episodes of
+# given link in a dictionary
 # {status:indicates sucess or failure, links:[]}     
 def getDownloadlinks(link):
+
     browser = SetupSelenium()
     resp = {
             "status": "",
             "links": []
         }
     wait = WebDriverWait(browser, 30)
+    
     if login(browser):
-        print("link:"+link)
+
         try:
-            browser.get("https://kissanime.ru"+link)
+
+            # Finds the episode list and appends to the list
+            browser.get(KISSANIME_URL+link)
             soup = BeautifulSoup(browser.page_source, 'html.parser')
             if len(soup.find_all('table')) == 0:
                 return None
             table = soup.find_all('table')[0]
             links = table.find_all('a')
-            print(links)
             episode_list = []
             for link in links:
                 episode = {
@@ -139,32 +160,45 @@ def getDownloadlinks(link):
                 "name": link.getText().strip()
                 }
                 episode_list.append(episode)
-            print(episode_list)
 
-        
+            # Get the rapidvideo download links
             episode_download_list = []
             for ep in episode_list:
                 e = {}
-                print("fetching : "+"https://kissanime.ru"+ep["url"])
             
-                browser.get("https://kissanime.ru"+ep["url"])
-                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".specialButton")))
-                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,".specialButton")))
+                browser.get(KISSANIME_URL+ep["url"])
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                        ".specialButton")))
+
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,
+                                                        ".specialButton")))
+
                 browser.find_element_by_css_selector(".specialButton").click()
-                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"div#divDownload a")))
-                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"div#divDownload a")))
-                e["url"] = browser.find_element_by_css_selector("div#divDownload a").get_attribute('href')
+
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                        "div#divDownload a")))
+
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,
+                                                        "div#divDownload a")))
+
+                e["url"] = browser.find_element_by_css_selector(
+                                                    "div#divDownload a"
+                                                    ).get_attribute('href')
+
                 e["name"] = ep["name"]
                 episode_download_list.append(e)
+
             browser.quit()
             resp["status"] = "sucess"
             resp["links"] = episode_download_list
             return resp
+
         except TimeoutException as e:
             browser.quit()
             resp["status"] = "sucess"
             resp["links"] = episode_download_list
             return resp
+        
         except WebDriverException as e:
             browser.quit
             print("wrong url")
@@ -177,13 +211,9 @@ def getDownloadlinks(link):
         return resp
 
 if __name__ =="__main__":
-    Anime_list = SearchAnime("goblin slayer")
+    Anime_list = SearchAnime("overlord")
     print(Anime_list)
     download_links = getDownloadlinks(Anime_list['links'][0]['url'])
-    # download_links = getDownloadlinks("abcd")
     print(download_links)
-    # getDownloadlinks("/Anime/Goblin-Slayer-Dub")
-    # Anime_list2 = SearchAnime("hfgafush")
-	# print(Anime_list2)
     
 
