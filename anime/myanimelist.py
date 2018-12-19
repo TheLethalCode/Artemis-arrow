4e088b2ef6ec6b95338270bbaafe74c22823c43d
thelethalcode <kousshikraj.raj@gmail.com>
2018-12-19 13:19:15 +0530

    Updated myanimelist scrapper

--- anime/myanimelist.py
+++ anime/myanimelist.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import os
-
+import multiprocessing as mp
 #Function to get url and return the data to scrape from it
 
 ANIME_URL = "https://myanimelist.net/anime/"
 JSON_PATH = os.path.join(os.path.dirname(__file__),"anime.json")
+CONFIG_PATH = os.path.join(os.path.dirname(__file__),"config.txt")
 
 def make_soup(source):
 	soupdata=BeautifulSoup(source.content,'html.parser')
@@ -24,28 +25,37 @@ def info_anime(soup):
 	name=anime.text
 	anime_list["name"]=name
 	
-	# Extracting the rating 
+	# Extracting the rating
 	rating=soup.find(name="div",attrs={"class":"fl-l score"})
 	anime_list["rating"]=float(rating.text.strip())
 
 	# Extracting the description
-	des=soup.find(name="span",attrs={"itemprop":"description"})
-	description=des.text
-	anime_list["description"]=description
+	try:
+		des=soup.find(name="span",attrs={"itemprop":"description"})
+		description=des.text
+		anime_list["description"]=description
+	except AttributeError:
+		anime_list["description"]="None"
 
 	# Extracting the Rank
 	rank=soup.find(name="span",attrs={"class":"numbers ranked"})
 	num = rank.text.find("#")+1
-	anime_list["rank"]=int(rank.text[num:])
-
+	try:
+		anime_list["rank"]=int(rank.text[num:])
+	except ValueError:
+		anime_list["rank"]=-1
+		
 	# Extracting number of episodes
 	ep=soup.find(name="div",attrs={"class":"spaceit"})
 	num=ep.text.find(" ")+2
-	anime_list["episodes"]=int(ep.text[num:-3])
-
+	try:
+		anime_list["episodes"]=int(ep.text[num:-3])
+	except ValueError:
+		anime_list["episodes"]=0
+		
 	return anime_list
 
-def gen_list(start,end):
+def gen_list(start, end):
 
 	"""
 	Function that generates a list of anime and its detail from start to end
@@ -62,15 +72,20 @@ def gen_list(start,end):
 			soup = make_soup(r)
 			anime_ind[index]=info_anime(soup)
 
-		except requests.exceptions.HTTPError:
+		except requests.exceptions.HTTPError as err:
+			# print(err)
+			anime_ind[index]= "Not Found"
 			continue
 
-	return anime_ind	
+	return anime_ind
+
+# def update():
+# 	with open(CONFIG_PATH,'r') as config:
+
 
 if __name__ == '__main__':
-	
 	with open(JSON_PATH,'w') as json_file:
-		json.dump(gen_list(393,400),json_file)
+		json.dump(gen_list(1,1000),json_file)
 		json_file.close()
 	
 
