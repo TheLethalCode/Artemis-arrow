11f6a700c35c4d0121b2097143a630990fcd1217
thelethalcode <kousshikraj.raj@gmail.com>
2018-12-20 01:12:27 +0530

    Loaded dotenv files

--- anime/anime_download.py
+++ anime/anime_download.py
@@ -1,5 +1,6 @@
 import os
-from bs4 import BeautifulSoup                               
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv, find_dotenv                               
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -9,9 +10,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException, WebDriverException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
+load_dotenv(find_dotenv())
 usernameStr=os.getenv("KISSANIME_USERNAME")
 passwordStr=os.getenv("KISSANIME_PASSWORD")
 
